721e51ac1556033b4ed11c0c2a24b68d9e3aaef6
Anton Golov <jesyspa@gmail.com>
2014-02-24 19:45:31 +0100

    Bunch of bugfixes and minor expansions code-wise.

--- measure.cpp
+++ measure.cpp
@@ -6,7 +6,7 @@
 #include <random>
 #include <stdexcept>
 #include <unordered_map>
-#include <vector>
+#include <set>
 
 int const REPEAT_COUNT = 1'000;
 int const DATA_SIZE = 1'000'000;
@@ -27,36 +27,72 @@ void print_aligned(T&& t, std::ostream& os = std::cout) {
     os << std::setw(16) << std::forward<T>(t);
 }
 
+template<typename Map, typename RAIter>
+void fill_map(Map& map, RAIter begin, RAIter end) {
+    std::for_each(begin, end, [&](auto x) { map.insert(std::make_pair(x, x)); });
+}
+
 template<typename Map, typename Clock, typename RAIter>
-void eval_map(std::ostream& os, RAIter begin, RAIter end) {
-    time_unit t;
+void eval_map_insertion(std::ostream& os, RAIter begin, RAIter end) {
+#ifdef SKIP_INSERTION_MEASURE
+    return;
+#endif
+    time_unit t{};
     for (int i = 0; i < REPEAT_COUNT; ++i) {
         Map ins_map;
         t += time<Clock>([&]() {
-            std::for_each(begin, end, [&](auto x) { ins_map.insert(std::make_pair(x, x)); });
+            fill_map(ins_map, begin, end);
         });
     }
-    print_aligned(t.count()/REPEAT_COUNT);
+    print_aligned(t.count()/REPEAT_COUNT, os);
+}
+
+template<typename Map, typename Clock, typename RAIter>
+void eval_map_query(std::ostream& os, RAIter begin, RAIter end) {
+#ifdef SKIP_QUERY_MEASURE
+    return;
+#endif
+    time_unit t{};
     Map query_map;
-    std::for_each(begin, end, [&](auto x) { query_map.insert(std::make_pair(x, x)); });
+    fill_map(query_map, begin, end);
     t += time<Clock>([&]() {
         for (int i = 0; i < REPEAT_COUNT; ++i) {
             std::for_each(begin, end, [&](auto x) { query_map.find(x); });
         }
     });
-    print_aligned(t.count()/REPEAT_COUNT);
+    print_aligned(t.count()/REPEAT_COUNT, os);
+}
+
+template<typename Map, typename Clock, typename RAIter>
+void eval_map(std::ostream& os, RAIter begin, RAIter end) {
+    eval_map_insertion<Map, Clock>(os, begin, end);
+    eval_map_query<Map, Clock>(os, begin, end);
     os << '\n';
 }
 
 template<typename T>
-std::vector<T> random_subset(std::vector<T>& vec, std::size_t size) {
+std::set<T> random_subset(std::vector<T> const& vec, std::size_t size) {
     // This is inefficient but should work fine.
     if (size > vec.size())
         throw std::runtime_error("not enough data");
-    std::vector<T> result = vec;
-    std::shuffle(result.begin(), result.end(), rd);
-    result.resize(size);
+
+    std::set<T> result;
+    std::uniform_int_distribution<> dist(0, vec.size()-1);
+    while (result.size() < size)
+        result.insert(vec[dist(rd)]);
+
     return result;
+}
+
+template<typename Map, typename T>
+void eval_structure(std::ostream& os, std::string const& structure_name, std::vector<T> const& data) {
+    for (int i = 8; i < 20; ++i) {
+        auto subset = random_subset(data, 1 << i);
+        print_aligned(structure_name, os);
+        print_aligned(1 << i);
+        print_aligned("int");
+        eval_map<Map, std::chrono::high_resolution_clock>(os, subset.begin(), subset.end());
+    }
 }
 
 int main() {
@@ -66,21 +102,13 @@ int main() {
     print_aligned("structure");
     print_aligned("entries");
     print_aligned("key_complexity");
+#ifndef SKIP_INSERTION_MEASURE
     print_aligned("insert");
+#endif
+#ifndef SKIP_QUERY_MEASURE
     print_aligned("query");
+#endif
     std::cout << std::endl;
-    for (int i = 8; i < 20; ++i) {
-        auto subset = random_subset(data, 1 << i);
-        print_aligned("bst");
-        print_aligned(1 << i);
-        print_aligned("int");
-        eval_map<std::map<int, int>, std::chrono::high_resolution_clock>(std::cout, subset.begin(), subset.end());
-    }
-    for (int i = 8; i < 20; ++i) {
-        auto subset = random_subset(data, 1 << i);
-        print_aligned("hashmap");
-        print_aligned(1 << i);
-        print_aligned("int");
-        eval_map<std::unordered_map<int, int>, std::chrono::high_resolution_clock>(std::cout, subset.begin(), subset.end());
-    }
+    eval_structure<std::map<int, int>>(std::cout, "bst", data);
+    eval_structure<std::unordered_map<int, int>>(std::cout, "hashmap", data);
 }
